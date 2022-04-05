bdea56e6849733004a1c941d7f7bc45842b2270e
ShoReagan <74525081+ShoReagan@users.noreply.github.com>
2022-04-05 11:41:25 -0500

    Update pInPOut

--- pInPOut/pInPOut.v
+++ pInPOut/pInPOut.v
@@ -4,7 +4,7 @@ module pInPOut #(parameter N = 4)
 	input clear, load,
 	output reg [N-1:0] Q
 );
-	always @ (posedge load) begin
+	always @ (negedge load, negedge clear) begin
 		if(clear == 0)
 			Q = 4'b0000;
 		else
