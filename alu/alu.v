ab47cf56a87c09f006a7859071b2cdc81599240a
ShoReagan <shoreaganw@gmail.com>
2022-04-07 17:52:06 -0500

    lab8

--- alu/alu.v
+++ alu/alu.v
@@ -3,18 +3,18 @@ module alu
 	input [3:0] A, B,
 	input S0, S1,
 	output Cout, ovr,
-	output [3:0] R
+	output [3:0] R,
+	output [13:0] W
 );
-		if(S1 == 0)
-			twoCompAddSub(A, B, S0, R, Cout, ovr);
-		else
-			begin
-				if(S0 == 0)
-					assign R = A & B;
-				else
-					assign R = A ^ B;
-				assign Cout = 0;
-				assign ovr = 0;
-			end
-		twoComp(R[0], R[1], R[2], R[3]);
+wire [3:0] addSubR, andOrR;
+wire wireC, wireO;
+
+	twoCompAddSub(A, B, S0, addSubR, wireC, wireO);
+	assign andOrR = S0==0 ? A & B : A ^ B;
+	assign R = S1==0 ? addSubR : andOrR;
+	assign Cout = S1==0 ? wireC : 0;
+	assign ovr = S1==0 ? wireO : 0;
+
+	twoComp(R[0], R[1], R[2], R[3], W[0], W[1], W[2], W[3], W[4], W[5], W[6], W[7], W[8], W[9], 
+	W[10], W[11], W[12], W[13]);
 endmodule
